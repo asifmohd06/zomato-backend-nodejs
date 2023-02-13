8fc413429f6f0a733ba766e7a25abf68430ce67e
asif mohd <itsafakemail400@gmail.com>
2023-02-13 12:13:57 +0530

    changed origin url to netlify

--- app.js
+++ app.js
@@ -33,11 +33,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "https://zomato06.netlify.app",
-    ],
+    origin: ["https://zomato06.netlify.app"],
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
     credentials: true,
   })
