f12fe34d5a03e57933ad7ca499f148ef2c522b72
asif mohd <itsafakemail400@gmail.com>
2023-02-13 14:21:28 +0530

    updated origin

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.use((req, res, next) => {
   ) {
     res.header("Access-Control-Allow-Origin", origin);
   }
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
