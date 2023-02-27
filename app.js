bec6b2e4d76a4b07fb86e717f8ab880d209944f7
asif mohd <itsafakemail400@gmail.com>
2023-02-27 17:37:13 +0530

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const dbUrl = process.env.MONGO_DB_URL;
 const routes = require("./routes/routes");
 const client = require("./models/clients");
 const jwtSecret = process.env.TOKEN_SECRET;
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 //Authentication
 // const session = require("cookie-session");
