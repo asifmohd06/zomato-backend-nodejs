5296e94dfe92d8ae96679d0eb4654760272e33e3
asif mohd <itsafakemail400@gmail.com>
2023-05-13 00:51:04 +0530

    updated cors

--- app.js
+++ app.js
@@ -37,23 +37,23 @@ app.use(express.json());
 // );
 
 // app.set("trust proxy", 1);
-// app.use((req, res, next) => {
-//   const origin = req.headers.origin;
-//   if (
-//     origin === "https://zomato06.netlify.app" ||
-//     origin === "https://zomato-zar0.onrender.com" ||
-//     origin === "http://127.0.0.1:3000/"
-//   ) {
-//     res.header("Access-Control-Allow-Origin", origin);
-//   }
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-app.use(cors());
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (
+    origin === "https://zomato06.netlify.app" ||
+    origin === "https://zomato-zar0.onrender.com" ||
+    origin === "http://127.0.0.1:3000/"
+  ) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+// app.use(cors());
 app.use(async (req, res, next) => {
   try {
     const getTokenFrom = (req) => {
